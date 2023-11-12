db628a503e90b15db41b0bcc50b6781090cdf6f5
Igor121 <chernokalsky89@gmail.com>
2023-11-12 13:38:52 +0300

    a3y17lte: generate proprietary-files * modify extract-files.sh to patch some of our libs

--- extract-files.sh
+++ extract-files.sh
@@ -6,17 +6,18 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-#function blob_fixup() {
-#    case "${1}" in
-#        vendor/lib/libsample1.so)
-#            sed -i 's|/data/misc/sample1|/data/misc/sample2|g' "${2}"
-#            ;;
-#        vendor/lib64/libsample2.so)
-#            "${PATCHELF}" --remove-needed "libsample3.so" "${2}"
-#            "${PATCHELF}" --add-needed "libsample4.so" "${2}"
-#            ;;
-#    esac
-#}
+function blob_fixup() {
+    case "${1}" in
+        vendor/lib/hw/camera.exynos7870.so|vendor/lib/libexynoscamera.so|vendor/lib/libexynoscamera3.so)
+            "${PATCHELF}" --replace-needed "libcamera_client.so" "libcamera_metadata_helper.so" "${2}"
+            "${PATCHELF}" --replace-needed "libgui.so" "libgui_vendor.so" "${2}"
+            ;;
+        vendor/lib*/libsensorlistener.so)
+            "$PATCHELF" --add-needed "libshim_sensorndkbridge.so" "$2"
+            "$PATCHELF" --remove-needed "libandroid.so" "$2"
+            ;;
+    esac
+}
 
 # If we're being sourced by the common script that we called,
 # stop right here. No need to go down the rabbit hole.
@@ -26,7 +27,7 @@ fi
 
 set -e
 
-export DEVICE=a6lte
+export DEVICE=a3y17lte
 export DEVICE_COMMON=exynos7870-common
 export VENDOR=samsung
 
