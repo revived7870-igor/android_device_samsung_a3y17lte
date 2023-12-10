0a306cd14da1465d939583b1cb791721ed4f8257
Igor121 <chernokalsky89@gmail.com>
2023-12-10 23:11:05 +0300

    a3y17lte: import sec_touchkey.kl from gtaxl
    * rename it to comply with getevent
    * required to make touch buttons functional

--- device.mk
+++ device.mk
@@ -30,6 +30,10 @@ TARGET_SCREEN_WIDTH := 720
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(DEVICE_PATH)/overlay
 
+# Keylayouts
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/keylayout/sec_touchkey.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/sec_touchkey.kl
+
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media/media_profiles_V1_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml
